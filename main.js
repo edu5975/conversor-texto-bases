27268a1976e141786bc3d9ae586d1ca2c41868a2
edu5975 <49260158+edu5975@users.noreply.github.com>
2021-04-18 21:42:55 -0500

    conversiones listas

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-//String.fromCharCode(valor)
-//console.log(btoa("lacasaazul"));
-//console.log(atob("bGFjYXNhYXp1bA=="));
+if(getParameterByName("mensaje") != ""){    
+    mensaje = getParameterByName("mensaje");
+    base = getParameterByName("base");
+    conversorTB(atob(mensaje),base);
+}
 
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
@@ -9,24 +11,25 @@ function getParameterByName(name) {
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-function modificarEnlace(mensaje){
+function modificarEnlace(mensaje, base){
     document.getElementById("link").innerHTML = "Enviar mensaje";
-    document.getElementById("link").href = "./index.html?mensaje=" + mensaje;
+    document.getElementById("link").href = "./index.html?mensaje=" + mensaje + "&base="+base;
   }  
 
-function conversorAB(msg = null, base = 2){
+function conversorTB(msg = null, base = null){
     ban = false;
     if(msg==null){
-        var msg = document.getElementById("msg").value;
+        msg = document.getElementById("msg").value;        
+        base = document.getElementById("baseConvertir").value;
         ban = true;
     }
     console.log(msg)
+    console.log(base)
     var mensajeConvertido = [];
     for (let i = 0; i < msg.length; i++) {
         const caracter = msg.charAt(i);
         const ascii = msg.charCodeAt(i);
         const caracterConvertido = ascii.toString(base);
-        console.log("Caracter: " + caracter + " - ascii: " + ascii + " - Caracter convertido "+ caracterConvertido)
         mensajeConvertido.push({
             caracter,
             ascii,
@@ -36,18 +39,33 @@ function conversorAB(msg = null, base = 2){
     console.log(mensajeConvertido);
 
     var convertido = document.getElementById("convertido");
-    var contenido = "";
-    mensajeConvertido.forEach(item => {
-        contenido += item.caracterConvertido + " ";
-    });
+    var contenido = '';      
+    for (let i = 0, j = 0; i < mensajeConvertido.length; i++, j++) {
+        contenido += mensajeConvertido[i].caracterConvertido +' ';  
+    }
     convertido.innerHTML = contenido;
-
-    if(ban)
-        modificarEnlace(btoa(msg))
 }
 
-if(getParameterByName("mensaje") != ""){    
-    var convertido = document.getElementById("entradas").innerHTML="";
-    val = getParameterByName("mensaje");
-    conversorAB(atob(val));
+function conversorBT(msg = null, base = null){
+    ban = false;
+    if(msg==null){
+        msg = document.getElementById("msgBase").value.split(" ");        
+        base = document.getElementById("baseOrigen").value;
+        ban = true;
+    }
+    console.log(msg)
+    var mensajeConvertido = "";
+    for (let i = 0; i < msg.length; i++) {
+        const ascii = parseInt(msg[i], base);        
+        const caracterConvertido = String.fromCharCode(ascii);
+        console.log(ascii+" - "+caracterConvertido)
+        mensajeConvertido += caracterConvertido;
+    }
+    console.log(mensajeConvertido);
+
+    var convertido = document.getElementById("convertido");
+    convertido.innerHTML = mensajeConvertido;
+
+    if(ban)
+        modificarEnlace(btoa(msg),base)
 }
